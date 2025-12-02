4a42386ed9e6e4666fbee9cff1b6fc76aed9da4d
José Briceño <josea2102@gmail.com>
2025-12-01 21:52:01 -0400

    Agregada implementación del repositorio de SinglePlayerGame

--- src/lib/asyncGame/domain/repositories/SinglePlayerGameRepository.ts
+++ src/lib/asyncGame/domain/repositories/SinglePlayerGameRepository.ts
@@ -1,15 +1,17 @@
 import { SinglePlayerGame } from "../aggregates/SinglePlayerGame";
 import { SinglePlayerGameId } from "../valueObjects/asyncGamesVO";
-import { UserId } from "src/lib/kahoot/domain/valueObject/Quiz";
+import { QuizId, UserId } from "src/lib/kahoot/domain/valueObject/Quiz";
 
 export interface SinglePlayerGameRepository {
 
     save(game: SinglePlayerGame): Promise<void>;
 
-    findById(id: SinglePlayerGameId): Promise<SinglePlayerGame[] | null>;
+    findById(id: SinglePlayerGameId): Promise<SinglePlayerGame | null>;
 
-    findByPlayerId(id: UserId): Promise<SinglePlayerGame | null>;
+    findByPlayerId(id: UserId): Promise<SinglePlayerGame[] | null>;
 
-    delete(id: SinglePlayerGameId): Promise<SinglePlayerGame | null>;
+    findActiveGameByPlayerAndQuiz(playerId: UserId, quizId: QuizId): Promise<SinglePlayerGame | null>;
+
+    delete(id: SinglePlayerGameId): Promise<void>;
 
 }
