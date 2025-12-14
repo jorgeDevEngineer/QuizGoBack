892e3af8cca9d8ea9ddcddad2fe5d512d48e2bc0
dan25g <dangar452000@gmail.com>
2025-12-14 00:42:07 -0400

    Fix: Docker ya carga la data existente

---
+++ docker/seed-data/08-user_favorite_quizzes.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.user_favorite_quizzes (user_id, quiz_id) VALUES ('33db2a9f-ed60-4161-ab98-b7cd8b079fa7', 'c5377f1d-0405-4bc7-8557-9d62221741da');
+INSERT INTO public.user_favorite_quizzes (user_id, quiz_id) VALUES ('33db2a9f-ed60-4161-ab98-b7cd8b079fa7', '5df5c581-379c-4f57-97c7-5d6dbf5c4da6');
