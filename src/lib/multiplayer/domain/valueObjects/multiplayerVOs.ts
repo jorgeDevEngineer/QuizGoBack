cf1a3b76546a3646517291ef34305c70bc78077e
José Briceño <josea2102@gmail.com>
2025-12-31 15:33:47 -0400

    VO: Id de player

--- src/lib/multiplayer/domain/valueObjects/multiplayerVOs.ts
+++ src/lib/multiplayer/domain/valueObjects/multiplayerVOs.ts
@@ -1,30 +1,26 @@
-import { randomUUID } from "crypto";
-
-const UUID_V4_REGEX = /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
-function isValidUUID(value: string): boolean {
-    return UUID_V4_REGEX.test(value);
-}
+import { UuidGenerator } from "src/lib/shared/domain/ports/UuuidGenerator";
 
 /**
  * Encapsula un identificador para un jugador de una sesión multijugador (solo le interesa a la sesión así que no está en shared)
  */
 export class PlayerId {
+    
+    private constructor(private readonly playerId: string) {}
 
-    private constructor(private readonly playerId:string){
-        if(!isValidUUID(playerId)){
+    public static of(playerId: string, uuidGenerator: UuidGenerator): PlayerId {
+        if (!uuidGenerator.isValid(playerId)) {
             throw new Error(`PlayerId does not have a valid UUID v4 format: ${playerId}`);
         }
-    }
-
-    public static of(playerId: string): PlayerId{
         return new PlayerId(playerId);
     }
 
-    public static generate(): PlayerId{
-        return new PlayerId(randomUUID());
+    public static generate(uuidGenerator: UuidGenerator): PlayerId {
+        const generatedId = uuidGenerator.generate();
+        return new PlayerId(generatedId);
     }
 
-    public getId():string{
+    public getId(): string {
         return this.playerId;
     }
+
 }
