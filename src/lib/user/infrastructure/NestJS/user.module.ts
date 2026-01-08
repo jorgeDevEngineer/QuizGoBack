93e9b6c2fa6c3abaee9c5a400be506c7e85a5bae
Diego Garcia <diego.frnz.2004@gmail.com>
2026-01-07 21:19:19 -0400

    Ajuste de formato en importes

--- src/lib/user/infrastructure/NestJS/user.module.ts
+++ src/lib/user/infrastructure/NestJS/user.module.ts
@@ -13,7 +13,10 @@ import { EnablePremiumMembershipCommandHandler } from "../../application/Handler
 import { EnableFreeMembershipCommandHandler } from "../../application/Handlers/Commands/EnableFreeMembershipCommandHandler";
 import { ErrorHandlingDecorator } from "src/lib/shared/aspects/error-handling/application/decorators/error-handling.decorator";
 import { LoggingUseCaseDecorator } from "src/lib/shared/aspects/logger/application/decorators/logging.decorator";
-import { ILoggerPort, LOGGER_PORT } from "src/lib/shared/aspects/logger/domain/ports/logger.port";
+import {
+  ILoggerPort,
+  LOGGER_PORT,
+} from "src/lib/shared/aspects/logger/domain/ports/logger.port";
 import { LoggerModule } from "src/lib/shared/aspects/logger/infrastructure/logger.module";
 
 @Module({
@@ -28,8 +31,16 @@ import { LoggerModule } from "src/lib/shared/aspects/logger/infrastructure/logge
       provide: GetAllUsersQueryHandler,
       useFactory: (logger: ILoggerPort, repository: TypeOrmUserRepository) => {
         const useCase = new GetAllUsersQueryHandler(repository);
-        const withErrorHandling = new ErrorHandlingDecorator(useCase, logger, 'GetAllUsersQueryHandler');
-        return new LoggingUseCaseDecorator(withErrorHandling, logger, 'GetAllUsersQueryHandler');
+        const withErrorHandling = new ErrorHandlingDecorator(
+          useCase,
+          logger,
+          "GetAllUsersQueryHandler"
+        );
+        return new LoggingUseCaseDecorator(
+          withErrorHandling,
+          logger,
+          "GetAllUsersQueryHandler"
+        );
       },
       inject: [LOGGER_PORT, "UserRepository"],
     },
@@ -37,8 +48,16 @@ import { LoggerModule } from "src/lib/shared/aspects/logger/infrastructure/logge
       provide: GetOneUserByIdQueryHandler,
       useFactory: (logger: ILoggerPort, repository: TypeOrmUserRepository) => {
         const useCase = new GetOneUserByIdQueryHandler(repository);
-        const withErrorHandling = new ErrorHandlingDecorator(useCase, logger, 'GetOneUserByIdQueryHandler');
-        return new LoggingUseCaseDecorator(withErrorHandling, logger, 'GetOneUserByIdQueryHandler');
+        const withErrorHandling = new ErrorHandlingDecorator(
+          useCase,
+          logger,
+          "GetOneUserByIdQueryHandler"
+        );
+        return new LoggingUseCaseDecorator(
+          withErrorHandling,
+          logger,
+          "GetOneUserByIdQueryHandler"
+        );
       },
       inject: [LOGGER_PORT, "UserRepository"],
     },
@@ -46,8 +65,16 @@ import { LoggerModule } from "src/lib/shared/aspects/logger/infrastructure/logge
       provide: GetOneUserByUserNameQueryHandler,
       useFactory: (logger: ILoggerPort, repository: TypeOrmUserRepository) => {
         const useCase = new GetOneUserByUserNameQueryHandler(repository);
-        const withErrorHandling = new ErrorHandlingDecorator(useCase, logger, 'GetOneUserByUserNameQueryHandler');
-        return new LoggingUseCaseDecorator(withErrorHandling, logger, 'GetOneUserByUserNameQueryHandler');
+        const withErrorHandling = new ErrorHandlingDecorator(
+          useCase,
+          logger,
+          "GetOneUserByUserNameQueryHandler"
+        );
+        return new LoggingUseCaseDecorator(
+          withErrorHandling,
+          logger,
+          "GetOneUserByUserNameQueryHandler"
+        );
       },
       inject: [LOGGER_PORT, "UserRepository"],
     },
@@ -55,8 +82,16 @@ import { LoggerModule } from "src/lib/shared/aspects/logger/infrastructure/logge
       provide: CreateUserCommandHandler,
       useFactory: (logger: ILoggerPort, repository: TypeOrmUserRepository) => {
         const useCase = new CreateUserCommandHandler(repository);
-        const withErrorHandling = new ErrorHandlingDecorator(useCase, logger, 'CreateUserCommandHandler');
-        return new LoggingUseCaseDecorator(withErrorHandling, logger, 'CreateUserCommandHandler');
+        const withErrorHandling = new ErrorHandlingDecorator(
+          useCase,
+          logger,
+          "CreateUserCommandHandler"
+        );
+        return new LoggingUseCaseDecorator(
+          withErrorHandling,
+          logger,
+          "CreateUserCommandHandler"
+        );
       },
       inject: [LOGGER_PORT, "UserRepository"],
     },
@@ -64,8 +99,16 @@ import { LoggerModule } from "src/lib/shared/aspects/logger/infrastructure/logge
       provide: DeleteUserCommandHandler,
       useFactory: (logger: ILoggerPort, repository: TypeOrmUserRepository) => {
         const useCase = new DeleteUserCommandHandler(repository);
-        const withErrorHandling = new ErrorHandlingDecorator(useCase, logger, 'DeleteUserCommandHandler');
-        return new LoggingUseCaseDecorator(withErrorHandling, logger, 'DeleteUserCommandHandler');
+        const withErrorHandling = new ErrorHandlingDecorator(
+          useCase,
+          logger,
+          "DeleteUserCommandHandler"
+        );
+        return new LoggingUseCaseDecorator(
+          withErrorHandling,
+          logger,
+          "DeleteUserCommandHandler"
+        );
       },
       inject: [LOGGER_PORT, "UserRepository"],
     },
@@ -73,8 +116,16 @@ import { LoggerModule } from "src/lib/shared/aspects/logger/infrastructure/logge
       provide: EditUserCommandHandler,
       useFactory: (logger: ILoggerPort, repository: TypeOrmUserRepository) => {
         const useCase = new EditUserCommandHandler(repository);
-        const withErrorHandling = new ErrorHandlingDecorator(useCase, logger, 'EditUserCommandHandler');
-        return new LoggingUseCaseDecorator(withErrorHandling, logger, 'EditUserCommandHandler');
+        const withErrorHandling = new ErrorHandlingDecorator(
+          useCase,
+          logger,
+          "EditUserCommandHandler"
+        );
+        return new LoggingUseCaseDecorator(
+          withErrorHandling,
+          logger,
+          "EditUserCommandHandler"
+        );
       },
       inject: [LOGGER_PORT, "UserRepository"],
     },
@@ -82,8 +133,16 @@ import { LoggerModule } from "src/lib/shared/aspects/logger/infrastructure/logge
       provide: EnablePremiumMembershipCommandHandler,
       useFactory: (logger: ILoggerPort, repository: TypeOrmUserRepository) => {
         const useCase = new EnablePremiumMembershipCommandHandler(repository);
-        const withErrorHandling = new ErrorHandlingDecorator(useCase, logger, 'EnablePremiumMembershipCommandHandler');
-        return new LoggingUseCaseDecorator(withErrorHandling, logger, 'EnablePremiumMembershipCommandHandler');
+        const withErrorHandling = new ErrorHandlingDecorator(
+          useCase,
+          logger,
+          "EnablePremiumMembershipCommandHandler"
+        );
+        return new LoggingUseCaseDecorator(
+          withErrorHandling,
+          logger,
+          "EnablePremiumMembershipCommandHandler"
+        );
       },
       inject: [LOGGER_PORT, "UserRepository"],
     },
@@ -91,8 +150,16 @@ import { LoggerModule } from "src/lib/shared/aspects/logger/infrastructure/logge
       provide: EnableFreeMembershipCommandHandler,
       useFactory: (logger: ILoggerPort, repository: TypeOrmUserRepository) => {
         const useCase = new EnableFreeMembershipCommandHandler(repository);
-        const withErrorHandling = new ErrorHandlingDecorator(useCase, logger, 'EnableFreeMembershipCommandHandler');
-        return new LoggingUseCaseDecorator(withErrorHandling, logger, 'EnableFreeMembershipCommandHandler');
+        const withErrorHandling = new ErrorHandlingDecorator(
+          useCase,
+          logger,
+          "EnableFreeMembershipCommandHandler"
+        );
+        return new LoggingUseCaseDecorator(
+          withErrorHandling,
+          logger,
+          "EnableFreeMembershipCommandHandler"
+        );
       },
       inject: [LOGGER_PORT, "UserRepository"],
     },
